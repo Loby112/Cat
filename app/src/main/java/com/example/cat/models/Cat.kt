07fe3f7a38d08a2f48851dc6bc450504fcfff09a
Loby112 <tobi195h@edu.zealand.dk>
2022-10-13 14:50:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/cat/models/Cat.kt
@@ -0,0 +1,9 @@
+package com.example.cat.models
+
+data class Cat(val id: Int, val name: String, val description: String,
+               val place: String, val reward: Int, val userId: String,
+               val date: Int, val pictureUrl: String) {
+    override fun toString(): String {
+        return "Name $name, Description $description, place $place, reward $reward, date: $date"
+    }
+}
---
+++ app/src/main/java/com/example/cat/models/CatViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.cat.models
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.example.cat.repository.CatRepository
+
+class CatViewModel: ViewModel() {
+    private val repository = CatRepository()
+    val catLiveData: LiveData<List<Cat>> = repository.catLiveData
+    val errorMessageLiveData: LiveData<String> = repository.errorMessageLiveData
+    val updateMessageLiveData: LiveData<String> = repository.updateMessageLiveData
+
+    init {
+        reload()
+    }
+
+    fun reload(){
+        repository.getPosts()
+    }
+
+    operator fun get(index: Int): Cat? {
+        return catLiveData.value?.get(index)
+    }
+
+    fun add(cat: Cat){
+        repository.add(cat)
+    }
+
+    fun delete(id: Int) {
+        repository.delete(id)
+    }
+
+    fun update(cat: Cat) {
+        repository.update(cat)
+    }
+}
